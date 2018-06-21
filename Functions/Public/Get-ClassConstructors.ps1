1e6ac51212ba19622a6231d5a174ec9712a95d66
Van Gulick Stéphane <Stephane.VanGulick@swisscom.com>
2018-06-22 00:10:35 +0200

    Added comment based help

--- Functions/Public/Get-ClassConstructors.ps1
+++ Functions/Public/Get-ClassConstructors.ps1
@@ -1,4 +1,27 @@
 Function Get-ClassConstructors{
+    <#
+    .SYNOPSIS
+        This function returns all existing constructors of a specific powershell class.
+    .DESCRIPTION
+        The Powershell Class must be loaded in memory for this function to work.
+    .EXAMPLE
+         Get-ClassConstructors -ClassName woop
+
+        Name ReturnType Properties
+        ---- ---------- ----------
+        woop woop
+        woop woop       {String, Number}
+    .INPUTS
+        String
+    .OUTPUTS
+        ClassConstructor
+    .NOTES   
+        Author: Stéphane van Gulick
+        Version: 0.7.1
+        www.powershellDistrict.com
+        Report bugs or submit feature requests here:
+        https://github.com/Stephanevg/PowerShellClassUtils
+    #>
     [cmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
--- Functions/Public/Get-ClassMethods.ps1
+++ Functions/Public/Get-ClassMethods.ps1
@@ -1,4 +1,32 @@
 Function Get-ClassMethods{
+    <#
+    .SYNOPSIS
+        This function returns all existing methods of a specific powershell class.
+    .DESCRIPTION
+        The Powershell Class must be loaded in memory for this function to work.
+    .EXAMPLE
+
+         Get-ClassMethods -ClassName wap
+
+        Name          ReturnType Properties
+        ----          ---------- ----------
+        DoChildthing  void
+        DoChildthing2 void       {Prop1, Prop2}
+        DoChildthing3 string     {Prop1, Prop2, Prop3}
+        DoChildthing4 bool       {Prop1, Prop2, Prop3}
+        DoSomething   string     {Prop1, Prop2, Prop3}
+
+    .INPUTS
+        String
+    .OUTPUTS
+        ClassMethod
+    .NOTES   
+        Author: Stéphane van Gulick
+        Version: 0.7.1
+        www.powershellDistrict.com
+        Report bugs or submit feature requests here:
+        https://github.com/Stephanevg/PowerShellClassUtils
+    #>
     [cmdletBinding()]
     Param(
         [Parameter(Mandatory=$true)]
