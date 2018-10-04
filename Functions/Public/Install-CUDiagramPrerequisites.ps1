94a81678b9bddd85688bb5527b41cc482a2591c0
Kevin Bates <31886373+bateskevin@users.noreply.github.com>
2018-10-04 16:42:08 +0200

    Update Install-CUDiagramPrerequisites.ps1

--- Functions/Public/Install-CUDiagramPrerequisites.ps1
+++ Functions/Public/Install-CUDiagramPrerequisites.ps1
@@ -1,16 +1,21 @@
 function Install-CUDiagramPrerequisites {
     [CmdletBinding()]
     param (
-        
+        [String]$Proxy        
     )
     
     if(!(Get-Module -Name PSGraph)){
         #Module is not loaded
         if(!(get-module -listavailable -name psgraph )){
-            write-verbose "Install PSGraph"
-            Install-Module psgraph -Verbose
-            Import-Module psgraph -Force
-            
+            if($proxy){
+                write-verbose "Install PSGraph"
+                Install-Module psgraph -Verbose -proxy $proxy
+                Import-Module psgraph -Force
+            }else{
+                write-verbose "Install PSGraph"
+                Install-Module psgraph -Verbose
+                Import-Module psgraph -Force
+            }
         }else{
             Import-Module psgraph -Force
         }
