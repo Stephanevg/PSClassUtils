1fc034c631fec3f0841a3ff8a5d19c327a86a75f
Philippe tulesauraspas <yanaski@msn.com>
2018-10-06 10:52:37 +0200

    minor changes

--- Functions/Public/Write-CUClassDiagram.ps1
+++ Functions/Public/Write-CUClassDiagram.ps1
@@ -1,69 +1,4 @@
-Function get-test {
-    <#
-    .SYNOPSIS
-        This script allows to document automatically existing script(s)/module(s) containing classes by generating the corresponding UML Diagram.
-    .DESCRIPTION
-        Automatically generate a UML diagram of scripts/Modules that contain powershell classes.
-
-    .PARAMETER Path
-
-    The path that contains the classes that need to be documented. 
-    The path parameter should point to either a .ps1 and .psm1 file.
-
-    .PARAMETER ExportFolder
-
-    This optional parameter, allows to specifiy an alternative export folder. By default, the diagram is created in the same folder as the source file.
-
-    .PARAMETER OutputFormat
-
-        Using the parameter OutputFormat, it is possible change the default output format (.png) to one of the following ones:
-
-        'jpg', 'png', 'gif', 'imap', 'cmapx', 'jp2', 'json', 'pdf', 'plain', 'dot'
-
-    .PARAMETER Show
-
-    Open's the generated diagram immediatly
-
-    .PARAMETER IgnoreCase
-        By default, Class names MUST be case identical to have the Write-CUClassDiagram cmdlet generate the correct inheritence tree.
-        When the switch -IgnoreCase is specified, All class names will be converted to 'Titlecase' to force the case, and ensure the inheritence is correctly drawed in the Class Diagram.
-    
-    .PARAMETER PassThru
-        When specified, the raw Graph inn GraphViz format will be returned back in String format.
-
-    .EXAMPLE
-
-    #Generate a UML diagram of the classes located in MyClass.Ps1
-    # The diagram will be automatically created in the same folder as the file that contains the classes (C:\Classes).
-
-    Write-CUClassDiagram.ps1 -File C:\Classes\MyClass.ps1
-
-    .EXAMPLE
-        #Various output formats are available using the parameter "OutPutFormat"
-
-        Write-CUClassDiagram.ps1 -File C:\Classes\Logging.psm1 -ExportFolder C:\admin\ -OutputFormat gif
-
-
-        Directory: C:\admin
-
-
-    Mode                LastWriteTime         Length Name
-    ----                -------------         ------ ----
-    -a----       12.06.2018     07:47          58293 Logging.gif
-
-    .EXAMPLE
-
-    Write-CUClassDiagram -FolderPath "C:\Modules\PSClassUtils\Classes\Private\" -Show
-
-    Will generate a diagram of all the private classes available in the FolderPath specified, and immediatley how the diagram.
-
-    .NOTES
-        Author: Stéphane van Gulick
-        Version: 0.8.2
-        www: www.powershellDistrict.com
-        Report bugs or ask for feature requests here:
-        https://github.com/Stephanevg/Write-CUClassDiagram
-    #>
+Function Write-CUClassDiagram {
     Param(
         [Alias("Name")]
         [Parameter(Mandatory=$true,ParameterSetName='File',ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
@@ -127,7 +62,7 @@ Function get-test {
             ## Make sure current file extension is either .ps1 or .psm1
             If ( $PsItem.Extension -in ('.ps1','.psm1')){
                 ## Fetch current item fullname
-                $Item = get-item $PSitem.fullName
+                $Item = Get-Item $PSitem.fullName
                 $ScriptFactory.Invoke()
             }
 
@@ -146,7 +81,7 @@ Function get-test {
                     If ( $PSBoundParameters['Show']) {$RecurseParam.add("Show",$PSBoundParameters['Show'])}
 
                     ## Do Recurse
-                    Get-ChildItem -Path $Path -Recurse | get-test @RecurseParam
+                    Get-ChildItem -Path $Path -Recurse | Write-CUClassDiagram @RecurseParam
 
                 } Else {
                     ## Path is a file, so we cannot recurse on that
@@ -154,7 +89,7 @@ Function get-test {
                 }
             } Else {
             ## Recurse Param is not used
-                $Item = get-item $Path
+                $Item = Get-Item $Path
                 ## Make sure current file extension is either .ps1 or .psm1
                 If ( $Item.Extension -in ('.ps1','.psm1')){
                     $ScriptFactory.Invoke()
