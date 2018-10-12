f8aee8ccb7ea6663b22a17ee30bc5156a5795028
stephanevg <svangulick@gmail.com>
2018-10-12 22:58:58 +0200

    Updated CI

--- CI/03_Tests.ps1
+++ CI/03_Tests.ps1
@@ -21,17 +21,25 @@ if ($res.FailedCount -gt 0 -or $res.PassedCount -eq 0) {
 
 
     
-if($res.FailedCount -eq 0 -and $env:APPVEYOR_REPO_BRANCH -eq "master"){
-    import-module $ModuleName -force
-    $GalleryVersion = (Find-Module $ModuleName).version
-    $LocalVersion = (get-module $ModuleName).version.ToString()
-    if($Localversion -le $GalleryVersion){
-        Write-host "[$($env:APPVEYOR_REPO_BRANCH)] PsClassUtils version $($localversion)  is identical with the one on the gallery. No upload done."
-    }Else{
+if ($res.FailedCount -eq 0 -and $res.successcount -ne 0) {
+    If ($env:APPVEYOR_REPO_BRANCH -eq "master") {
+        Write-host "[$($env:APPVEYOR_REPO_BRANCH)] All tested Passed, and on Branch 'master'"
+        import-module $($env:APPVEYOR_BUILD_FOLDER)\$($ModuleName).psd1 -force
+        $GalleryVersion = (Find-Module $ModuleName).version
+        $LocalVersion = (get-module $ModuleName).version.ToString()
+        if ($Localversion -le $GalleryVersion) {
+            Write-host "[$($env:APPVEYOR_REPO_BRANCH)] PsClassUtils version $($localversion)  is identical with the one on the gallery. No upload done."
+            write-host "[$($env:APPVEYOR_REPO_BRANCH)] Module not deployed to the psgallery" -foregroundcolor Yellow;
+        }
+        Else {
 
-    publish-module -Name $ModuleName -NuGetApiKey $Env:PSgalleryKey;
-    write-host "[$($env:APPVEYOR_REPO_BRANCH)] Module deployed to the psgallery" -foregroundcolor green;
+            publish-module -Name $ModuleName -NuGetApiKey $Env:PSgalleryKey;
+            write-host "[$($env:APPVEYOR_REPO_BRANCH)] Module deployed to the psgallery" -foregroundcolor green;
+        }
+    }Else{
+        Write-host "[$($env:APPVEYOR_REPO_BRANCH)] Awesome, nothing to do more. If you want to upload to the gallery, please merge from dev into master"
     }
-}else{
-    write-host "[$($env:APPVEYOR_REPO_BRANCH)]Module not deployed to the psgallery" -foregroundcolor Yellow;
+}
+else {
+    Write-host "[$($env:APPVEYOR_REPO_BRANCH)] Failed tests: $($res.failedcount) - Successfull tests: $($res.successcount)" -ForegroundColor Red
 }
