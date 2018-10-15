bb12dd24e02b562a9ff406695474dd099cfbb41f
LxLeChat <43340127+LxLeChat@users.noreply.github.com>
2018-10-15 16:59:45 +0200

    Add property ClassName to ASTDocument class
    Add property ClassName to ASTDocument class

--- PsClassUtils/Classes/Private/04_ASTDocument.ps1
+++ PsClassUtils/Classes/Private/04_ASTDocument.ps1
@@ -2,10 +2,12 @@ Class ASTDocument {
     [System.Management.Automation.Language.StatementAst[]]$Classes
     [System.Management.Automation.Language.StatementAst[]]$Enums
     $Source
+    $ClassName
 
     ASTDocument ([System.Management.Automation.Language.StatementAst[]]$Classes,[System.Management.Automation.Language.StatementAst[]]$Enums,$Source){
         $This.Classes = $Classes
         $This.Enums = $Enums
         $This.Source = $Source
+        $This.ClassName = $Classes.Name
     }
 }
