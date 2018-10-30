60ea02dccd3de281e3b5857e7f4712564c4c9b8d
LxLeChat <43340127+LxLeChat@users.noreply.github.com>
2018-10-30 16:11:16 +0100

    Updated CUClass and Get-CUClass

--- PSClassUtils/Classes/Private/06_CUClass.ps1
+++ PSClassUtils/Classes/Private/06_CUClass.ps1
@@ -7,55 +7,58 @@ Class CUClass {
     Hidden $Ast
 
     CUClass($RawAST){
+
         $this.Raw = $RawAST
         $this.Ast = $this.Raw.FindAll( {$args[0] -is [System.Management.Automation.Language.TypeDefinitionAst]}, $true)
-        $This.SetPropertiesFromRawAST($this.Raw)
+        $This.SetPropertiesFromRawAST()
+
     }
 
     CUClass ($Name,$Property,$Constructor,$Method){
+
         $This.Name = $Name
         $This.Property = $Property
         $This.Constructor = $Constructor
         $This.Method = $Method
+
     }
+
     CUClass ($Name,$Property,$Constructor,$Method,$RawAST){
+
         $This.Name = $Name
         $This.Property = $Property
         $This.Constructor = $Constructor
         $This.Method = $Method
         $This.Raw = $RawAST
-    }
-
-    [void] SetPropertiesFromRawAST($RawAST){
-        $this.Name = $RawAST.Name
-        ## remplacer par: SetConstructorsFromRawAST !!!! c est ça que je trouve chelouuUUuu!
-        #$this.Constructor = Get-CUClassConstructor -ClassName $this.Name -InputObject $RawAST
-        $this.Method = Get-CUClassMethod -InputObject $RawAST -ClassName $this.Name # ?
-        #En faite, au sein de la class, CUClass, comme c'est la base de toutes les class (un peu comme l'anneau unique) Il vaut mieux ne pas la rendre dépendante d'autres cmdlets
-        # J'ai eu ça une fois sur un module xunitxml quq javais ecris. Et finalement, j'avais une dependance circulaire. Apres, pour loader les classes ct chaud, car tu dois les loader dans l'ordre
-        #Et si elle dependent l'une de l'autre, c'est caca.
-        #C#a#r si on laisse Get-CUClassMEthod ici, ben Get-CUClassMEthod elle fais quoi? $obj = Get-CUClass -ClassName; $obj.Method , tu te rapelles?
-        #Dependence circulaire ! 
-        ##mais oui c'est exactement ça qui me turlupinait!
-        #lol, on a ecris bien plus de commentaires que de code finalement :D
-
-        # t enocore la? dsl ma ptite pleure c galère !
-        $this.Property = Get-CUClassProperty -InputObject $RawAST -ClassName $this.Name
-        $this.SetConstructorsFromRawAST()
 
     }
 
-    #du coup j'ai fait ça 
-    [void] SetConstructorsFromRawAST(){
+    ## Set Name, and call Other Set
+    [void] SetPropertiesFromRawAST(){
+
+        $This.Name = $This.Ast.Name
+        $This.SetConstructorFromAST()
+        $This.SetPropertyFromAST()
+        $This.SetMethodFromAST()
+
+    }
+
+    ## Find Constructors for the current Class
+    [void] SetConstructorFromAST(){
+        
         $Constructors = $null
-        $Constructors = $this.Ast | ? {$_.IsConstructor -eq $true}
-        foreach ( $Constructor in $Constructors){
+        $Constructors = $This.Ast.Members | Where-Object {$_.IsConstructor -eq $True}
+
+        Foreach ( $Constructor in $Constructors ) {
+
             $Parameters = $null
             $Parameters = $Constructor.Parameters
             [ClassProperty[]]$Paras = @()
-            If ($Parameters) {
+
+            If ( $Parameters ) {
                 
-                foreach ($Parameter in $Parameters) {
+                Foreach ( $Parameter in $Parameters ) {
+
                     $Type = $null
                     # couldn't find another place where the returntype was located. 
                     # If you know a better place, please update this! I'll pay you beer.
@@ -65,35 +68,83 @@ Class CUClass {
                 }
 
             }
-            #[ClassConstructor]::New($Constructor.Name, $Constructor.ReturnType, $Paras,$Constructor)
+
             $This.Constructor += [ClassConstructor]::New($Constructor.Name, $Constructor.ReturnType, $Paras,$Constructor)
         }
-    }
-
-    [void] SetMethodFromRawAST(){
-##du coup regarde
-    }
-
-    [void] SetPropertyFromRawAST(){
 
     }
 
+    ## Find Methods for the current Class
+    [void] SetMethodFromAST(){
+
+        $Methods = $null
+        $Methods = $This.Ast.Members | Where-Object {$_.IsConstructor -eq $False}
+
+        Foreach ( $Method in $Methods ) {
+
+            $Parameters = $null
+            $Parameters = $Method.Parameters
+            [ClassProperty[]]$Paras = @()
+
+            If ( $Parameters ) {
+                
+                Foreach ( $Parameter in $Parameters ) {
+
+                    $Type = $null
+                    # couldn't find another place where the returntype was located. 
+                    # If you know a better place, please update this! I'll pay you beer.
+                    $Type = $Parameter.Extent.Text.Split("$")[0] 
+                    $Paras += [ClassProperty]::New($Parameter.Name.VariablePath.UserPath, $Type)
+        
+                }
+
+            }
+
+            $This.Method += [ClassMethod]::New($Method.Name, $Method.ReturnType, $Paras,$Method)
+        }
+
+    }
+
+    ## Find Properties for the current Class
+    [void] SetPropertyFromAST(){
+
+        $Properties = $This.Ast.Members | Where-Object {$_ -is [System.Management.Automation.Language.PropertyMemberAst]} 
+
+        If ($Properties) {
+        
+            Foreach ( $Pro in $Properties ) {
+                
+                If ( $Pro.IsHidden ) {
+                    $Visibility = "Hidden"
+                } Else {
+                    $visibility = "public"
+                }
+            
+                $This.Property += [ClassProperty]::New($pro.Name, $pro.PropertyType.TypeName.Name, $Visibility,$Pro)
+            }
+        }
+
+    }
+
+    ## Return the content of Constructor
+    [ClassConstructor[]]GetCuClassConstructor(){
+
+        return $This.Constructor
+        
+    }
+
+    ## Return the content of Method
     [ClassMethod[]]GetCuClassMethod(){
 
-        return $this.Method
+        return $This.Method
+
     }
 
-    [ClassMethod[]]GetCuClassMethod([]){
+    ## Return the content of Property
+    [ClassProperty[]]GetCuClassProperty(){
 
-        return $this.Method
+        return $This.Property
+
     }
 
-    #>
-    #Mais c'est exactement ce que je pensais aussi. Je te l'a meme ecris lol
-    #Just avant ta gamere
-    <#
-        MAi oui effectivement, cela fais bcp plus de sense comme ça
-        
-    #>
-    #lol! j'ai rien pigé!
 }
