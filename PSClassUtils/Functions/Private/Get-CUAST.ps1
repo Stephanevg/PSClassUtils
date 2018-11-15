31c6114620ecce8fa4d70ad51a097a5ef80e657a
LxLeChat <yanaski@msn.com>
2018-11-15 21:02:04 +0100

    minor changes + fixing test

--- PSClassUtils/Functions/Private/Get-CUAST.ps1
+++ PSClassUtils/Functions/Private/Get-CUAST.ps1
@@ -11,8 +11,9 @@ Function Get-CUAst {
     process {
         foreach($p in $Path){
 
+            Write-Verbose "Current file $p"
             If ( $P.Extension -in '.ps1','.psm1') {
-                #[scriptblock]::Create( $(Get-Content -Path $P.FullName -Raw) ).Ast
+                Write-Verbose "Current file $p is a PS1 or PSM1 file..."
                 $Raw = [System.Management.Automation.Language.Parser]::ParseFile($p.FullName, [ref]$null, [ref]$Null)
                 $AST = $Raw.FindAll( {$args[0] -is [System.Management.Automation.Language.TypeDefinitionAst]}, $true)
 
@@ -20,15 +21,23 @@ Function Get-CUAst {
                 Switch ($AST.count) {
                     
                     { $AST.count -eq 1 } {
+                        Write-Verbose "Current file $p contains 1 AST..."
                         $AST
                     }
 
                     { $AST.count -gt 1 } {
+                        Write-Verbose "Current file $p contains $($ast.count) AST..."
                         Foreach ( $x in $AST ) {
                             $x
                         }
                     }
+
+                    Default {
+                        Write-Verbose "Current file $p contains $($ast.count) AST..."
+                    }
                 }
+            } Else {
+                Write-Verbose "Current file $p is not a PS1 or PSM1 file..."
             }
         }
     }
