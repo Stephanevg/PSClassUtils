f8b29015a2d26a7120814e71d349a8af3af12cfa
stephanevg <svangulick@gmail.com>
2019-03-11 08:10:35 +0100

    added GetParamHashTable. Added properties.

--- Code/Classes/08_CUDiagram.ps1
+++ Code/Classes/08_CUDiagram.ps1
@@ -1,3 +1,5 @@
+
+
 Class CUDiagram {
     [String]$Graph
     [CUClass[]]$Class
@@ -31,17 +33,105 @@ Class CUDiagram {
     CreateGraph(){
         #actions for creating graph
         If($this.Class -or $this.Enum){
+            $Pars = @{}
+            $Pars.InputObject = ""
+            $Pars.IgnoreCase = ""
+            $Pars.ShowCOmposition = ""
+            $Pars.IgnoreCase = ""
+            
+            try{
 
-            $this.Graph = "GRAPH HERE"
+                $this.Graph = Out-CUGraph @$Pars -ErrorAction -Stop
+            }Catch{
+                Throw "Failed to create raw graph: $($_)"
+            }
+            IF(!($this.Path)){
+                Throw "Generated graph is empty. Did you point to a document that contains classes / enums?"
+            }
         }Else{
             throw "Add classes or Enums"
         }
     }
-
+    
     CreateDiagram(){
         If(!($this.Graph)){
             Throw "Create a graph first using CreateGraph"
+        }else{
+            $ParsExport = @{}
+            $ParsExport.Show = ""
+            $ParsExport.PassThru = ""
+            $ParsExport.OutputFormat = ""
+            $this.Graph | export-PSGraph @ParsExport
+            
         }
     }
 
 }
+#>
+Enum GraphOutputFormat{
+    jpg
+    png
+    gif
+    imap
+    cmapx
+    jp2
+    pdf
+    plain
+    dot
+}
+
+Class ClassGraphOptions {
+    [Object]$InputObject #Show be [CUClass[]] and / or [CUEnum[]]
+    [bool]$IgnoreCase
+    [Bool]$ShowComposition
+    [Bool]$Show
+    [Bool]$PassThru
+    [GraphOutputFormat]$Format
+
+    ClassGraphOptions(){
+
+    }
+
+    [ClassGraphOptions] SetInputObject([Object]$InputObject){
+        $this.Object = $InputObject
+        return $this
+    }
+
+
+    [ClassGraphOptions] SetShowComposition(){
+        $this.ShowComposition = $true
+        return $this
+    }
+
+    [ClassGraphOptions] SetIgnoreCase(){
+        $this.IgnoreCase = $true
+        return $this
+    }
+
+    [ClassGraphOptions] SetShow(){
+        $this.Show = $true
+        return $this
+    }
+
+    [ClassGraphOptions] SetPassThru(){
+        $this.PassThru = $true
+        return $this
+    }
+
+    [ClassGraphOptions] SetOutputFormat([GraphOutputFormat]$Format){
+        $this.Format = $Format
+        return $this
+    }
+
+    [HashTable] GetParameterHashTable(){
+        
+        $Options = $this.psobject.Members | ? {$_.MemberType -eq 'Property'} | select Name,Value
+        $Hash = @{}
+        Foreach($opt in $options){
+            $Hash.$($opt.Name) = $opt.Value
+        }
+
+        return $Hash
+    }
+}
+
